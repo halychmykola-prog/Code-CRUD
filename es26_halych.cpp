fa1c5868c3d9f20d08c0200e619efd076a74abb9
halychmykola-prog <halych.mykola.studente@itispaleocapa.it>
2026-02-10 08:57:57 +0100

    Add files via upload
    Iniziato scrivere il codice

---
+++ es26_halych.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int vector[]
+	
+	
+	cout<<"Un codice crud per lavorare con gli array"<<endl;
+	
+	
+	do
+	{
+		cout<<"Se vuoi "
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}while(option!=0)
+
+    return 0;
+}
+
+/*(C) aggiunta
+(R) visualizzazione
+(U) modifica
+(D) cancellazione*/
