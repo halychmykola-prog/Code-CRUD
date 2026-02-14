1d544328fc3cb5509b8c8695dfd6758c183806ee
halychmykola-prog <halych.mykola.studente@itispaleocapa.it>
2026-02-14 11:03:06 +0100

    Add files via upload
    Aggiunto "cout" interface. Aggiunta funzione per visualizzare array(errata)

--- es26_halych.cpp
+++ es26_halych.cpp
@@ -1,27 +1,88 @@
 #include <iostream>
 using namespace std;
 
+void aggiunta(int v[], int dim)
+{
+    
+}
+
+void visualizzazione(int v[], int dim)
+{
+    cout<<"Array visualizzato:";
+    for(int i=0;i<dim;i++)
+    {
+        if(i>9 && i<20)
+            cout<<endl;
+        if(i>19 && i<30)
+            cout<<endl;
+        if(i>29 && i<40)
+            cout<<endl;
+        if(i>39 && i<50)
+            cout<<endl;
+            
+        cout<<" ["<<v[i]<<"]";
+        
+    }
+    
+    
+    
+}
+
+void modifica(int v[], int dim)
+{
+    
+}
+
+void cancellazione(int v[], int dim)
+{
+    
+}
+
+
+
+
+
+
 int main()
 {
-	int vector[]
+	int v[50] = {12, 45, 3, 67, 29, 54, 1, 70, 18, 39, 22, 60, 7, 41, 33, 9, 55, 26, 48, 14,
+               62, 5, 37, 20, 69, 11, 52, 30, 16, 44, 8, 63, 24, 58, 2, 35, 47, 13, 40, 21,
+               66, 10, 53, 28, 34, 59, 6, 42, 19, 50};
+	int dimension=sizeof(v)/sizeof(v[0]);
 	
 	
-	cout<<"Un codice crud per lavorare con gli array"<<endl;
 	
+	cout<<"Un codice crud per lavorare con gli array"<<endl<<endl;
+	char option=0;
 	
 	do
 	{
-		cout<<"Se vuoi "
+	    cout<<"Opzioni: "<<endl;
+		cout<<"   Per fare fare aggiunta, premi 'C'."<<endl<<"   Per visualizzare array, premi '2'."<<endl<<"   Per fare modifica veloce di array premi 'U'"<<endl;
+		cout<<"   Per cancellare un elemento di array premi 'D'."<<endl<<"   Per uscire dal menu premi '0' "<<endl<<endl;
+		
+		cout<<"Quale operazione vuoi fare? Inserisci la lettera o la cifra: ";
+		cin>>option;
+		
+		cout<<endl;
+		
+		
+		switch(option)
+		{
+		    case 2: 
+		        visualizzazione(v, dimension);
+		        break;
+		    
+		        
+		    
+		}
 		
 		
 		
 		
 		
 		
-		
-		
-		
-	}while(option!=0)
+	}while(option!=0);
 
     return 0;
 }
